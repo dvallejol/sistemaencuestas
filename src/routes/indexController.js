232b0e0f09416a6e5905b6e33250a3496e83b6f4
dijolev <diegovallejocei@gmail.com>
2024-06-10 21:39:44 -0500

    update user

--- src/routes/indexController.js
+++ src/routes/indexController.js
@@ -1,11 +1,26 @@
-//importamos el framework
-const express = require ('express');
+const express = require('express');
 const router = express.Router();
+const pool = require('../connection');
+const { paginator } = require('../lib/paginator');
+var url = require('url');
 
-router.get('/', (req, res)=>{
-    //res.send('Welcome to the API');
-    res.render('index/index');
+router.get('/',async(req,res)=>{
+    var listPoll;
+    let data = {};
+    var query = url.parse(req.url, true).query;
+    if (undefined == query.filtrar){
+        listPoll = await pool.query('SELECT * FROM polls', [0]);
+    }else{
+        listPoll = await pool.query('SELECT * FROM polls WHERE poll LIKE ?', ['%' +query.filtrar+ '%']);
+    }
+    if (0 < listPoll.length){
+        data = paginator(listPoll, req.query.pagina, 3, "/", "http://localhost:8080");
+    }else{
+        data = {
+            pagi_info: "No hay datos que mostrar",
+          };
+    }
+    res.render('index/index', { data });
 });
-
 module.exports = router;
 
